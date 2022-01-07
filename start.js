bcf9ce456fe6b0de6c4526c60186e49d57d4de28
Luis Otero <alonsooteroseminario@gmail.com>
2022-01-06 18:04:27 -0800

    update

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cookieSession = require('cookie-session');
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000;
 const config = require('./config');
